fe31850e1f79ce2fa6bc21fc27be5b0298450c40
Chaoran Zhou <zhouchaoran304@gmail.com>
2025-01-09 14:36:12 -0500

    Jan 9

--- src/app/about/bio/page.tsx
+++ src/app/about/bio/page.tsx
@@ -1,3 +1,14 @@
 export default function Bio() {
-  return <div></div>;
+  return (
+    <div>
+      <a 
+        href="/pdfs/Biography_Chaoran Zhou.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline"
+      >
+        BIOGRAPHY_CHAORAN ZHOU.PDF ↗
+      </a>
+    </div>
+  );
 }
