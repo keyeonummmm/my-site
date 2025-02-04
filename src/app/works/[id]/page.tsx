3bf38971dbb0a12c9e13127b5157dfaf9dd8485c
Chaoran Zhou <zhouchaoran304@gmail.com>
2025-02-04 15:38:04 -0500

    build bug fixed

--- src/app/works/[id]/page.tsx
+++ src/app/works/[id]/page.tsx
@@ -2,13 +2,11 @@ import WorkDetail from '@/app/ui/WorkDetail'
 import { worksList } from '../works'
 import { notFound } from 'next/navigation'
 
-interface PageProps {
-  params: {
-    id: string
-  }
-}
-
-export default async function WorkPage({ params }: PageProps) {
+export default async function WorkPage({
+  params,
+}: {
+  params: { id: string }
+}) {
   const work = worksList.find(w => w.id === params.id)
   
   if (!work) {
