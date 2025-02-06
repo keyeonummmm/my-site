6fdaca10ad6a25c1ed76198fc8f1e8dd47a98fa0
Chaoran Zhou <zhouchaoran304@gmail.com>
2025-02-06 17:19:03 -0500

    bio page and pdf update

--- src/app/about/bio/page.tsx
+++ src/app/about/bio/page.tsx
@@ -22,12 +22,22 @@ export default function Bio() {
         <h3>Born in 2002 in Xiamen, China.</h3>
         <h3>Lives and works in Toronto, ON, Canada.</h3>
         <br />
+        <p>
+        Chaoran Zhou is a Chinese-born artist and independent software developer currently pursuing a Bachelor of Fine Arts of Drawing and Painting at OCAD University in Toronto. 
+        Specializing in both conceptual installations and paintings with diverse visual expressions that connect audiences to thought-provoking concepts, and conveying a poetic visual experience.
+        </p>
+        <br />
+        <p>
+        As a start-up developer, Chaoran's software was designed with the initial intention of exploring the form of an online-to-offline collaborative community platform. He is also exploring artificial intelligence technologies.
+        </p>
+        <br />
         <div className="exhibition-catalogue">
           <h2>Group Exhibitions:</h2>
           <br />
           <h3>Map of Sceneries. 37 Baldwin St, Toronto. 2022</h3>
           <h3>A Breathing Room. OCAD University, Toronto. 2023</h3>
           <h3>anxious yet familiar world. Ignite Gallery, OCAD University, Toronto. 2025</h3>
+          <br />
           <br />
           <h2>Solo Exhibitions:</h2>
           <br />
